398d74b6c0dcd45c0015fea6a92ce27c87bec0bb
cxhello <caixiaohuichn@gmail.com>
2024-03-15 09:55:54 +0800

    Initial commit

---
+++ src/main/java/top/cxhello/sample/SpringCloudGatewaySampleApplication.java
@@ -0,0 +1,13 @@
+package top.cxhello.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGatewaySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGatewaySampleApplication.class, args);
+    }
+
+}
